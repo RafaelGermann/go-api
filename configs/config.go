163797c60834b78eeded771ef661bddd471d5c98
Rafael Germann <rafael.germann@gmail.com>
2025-06-10 22:14:19 -0300

    Initial Project

---
+++ configs/config.go
@@ -0,0 +1,35 @@
+package configs
+
+import (
+	"github.com/spf13/viper"
+)
+
+var cfg *config
+
+type config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBHost        string `mapstructure:"DB_HOST"`
+	DBPort        string `mapstructure:"DB_PORT"`
+	DBUser        string `mapstructure:"DB_USER"`
+	DBPass        string `mapstructure:"DB_PASS"`
+	DBName        string `mapstructure:"DB_NAME"`
+	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+}
+
+func LoadConfig(path string) (*config, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		panic(err)
+	}
+
+	return cfg, nil
+}
